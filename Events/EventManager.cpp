7513aac1a86348f9ebceebc728ecc8b8ba7cbdcf
wcobalt <cobalt.itech@gmail.com>
2018-09-15 14:23:36 +0300

    Added EventManager, added several consts about event-types.

---
+++ Events/EventManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wcobalt on 15.09.18.
+//
+
+#include "EventManager.h"
---
+++ Events/EventManager.h
@@ -0,0 +1,31 @@
+//
+// Created by wcobalt on 15.09.18.
+//
+
+#ifndef DENGINE_EVENTMANAGER_H
+#define DENGINE_EVENTMANAGER_H
+
+
+class EventManager {
+public:
+    const unsigned int EVENT_CLICK = 0;
+    const unsigned int EVENT_LEFT_BUTTON_CLICK = 1;
+    const unsigned int EVENT_RIGHT_BUTTON_CLICK = 2;
+    const unsigned int EVENT_MIDDLE_BUTTON_CLICK = 3;
+    const unsigned int EVENT_MOUSE_MOVE = 4;
+    const unsigned int EVENT_MOUSE_OVER = 5;
+    const unsigned int EVENT_MOUSE_OUT = 6;
+    const unsigned int EVENT_WINDOW_OPEN = 7;
+    const unsigned int EVENT_WINDOW_CLOSE = 8;
+    const unsigned int EVENT_WINDOW_MINIMIZE = 9;
+    const unsigned int EVENT_WINDOW_MAXIMIZE = 10;
+    const unsigned int EVENT_WINDOW_WINDOWED = 11;
+    const unsigned int EVENT_KEY_PRESS = 12;
+    const unsigned int EVENT_KEY_RELEASE = 13;
+
+    void setEventHandler(unsigned int event, EventHandler eventHandler);
+    EventHandler getEventManager(unsigned int event);
+};
+
+
+#endif //DENGINE_EVENTMANAGER_H
