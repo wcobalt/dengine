a1524c907dce4cd1b07a92573e8316413a504b35
wcobalt <cobalt.itech@gmail.com>
2018-09-30 12:59:31 +0300

    Frame of GameObject is written.

--- GameObject.cpp
+++ GameObject.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "GameObject.h"
+
+std::vector<Component> GameObject::getAllComponents() {
+
+}
+
+std::vector<Component> GameObject::getComponentsByID(ID id) {
+
+}
+
+void GameObject::addComponent(Component component) {
+
+}
+
+void GameObject::setParent(GameObject parent) {
+
+}
+
+void GameObject::addChild(std::vector<GameObject> children) {
+
+}
+
+GameObject GameObject::getParent() {
+
+}
+
+std::vector<GameObject> GameObject::getChildren() {
+
+}
+
+ID GameObject::getID() {
+
+}
--- GameObject.h
+++ GameObject.h
@@ -2,12 +2,26 @@
 // Created by wcobalt on 19.09.18.
 //
 
+#include "Components/Component.h"
+#include <vector>
+#include <string>
+
 #ifndef DENGINE_GAMEOBJECT_H
 #define DENGINE_GAMEOBJECT_H
 
 
 class GameObject {
-
+public:
+    std::vector<Component> getAllComponents();
+    std::vector<Component> getComponentsByID(ID id);
+    void addComponent(Component component);
+    void setParent(GameObject parent);
+    void addChild(std::vector<GameObject> children);
+    GameObject getParent();
+    std::vector<GameObject> getChildren();
+    ID getID();
+protected:
+    ID id;
 };
 
 
