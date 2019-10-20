8b404794fb93a167cfac6d89577649d9c99d0af1
wcobalt <cobalt.itech@gmail.com>
2019-10-20 15:13:50 +0300

    Streams/InputBitStream
    +Streams/InputBitStream was added and implemented in DefaultInputBitStream

---
+++ src/Utils/Streams/DefaultInputBitStream.cpp
@@ -0,0 +1,33 @@
+//
+// Created by wcobalt on 10/20/19.
+//
+
+#include "DefaultInputBitStream.h"
+
+using namespace dengine;
+
+DefaultInputBitStream::DefaultInputBitStream(const char *stream):stream(stream), pointer(0) {}
+
+bool DefaultInputBitStream::read() {
+    size_t bitNumber = pointer % BITS_IN_CHAR, byteNumber = pointer / BITS_IN_CHAR;
+    pointer++;
+
+    return ((stream[byteNumber] >> bitNumber) & 1) == 1;
+}
+
+unsigned long DefaultInputBitStream::readNumber(size_t size, bool isInverseOrder) {
+    unsigned long result = 0;
+
+    for (size_t i = 0; i < size; i++, pointer++) {
+        bool bit = read();
+
+        if (isInverseOrder)
+            result |= ((unsigned long)bit << i);
+        else {
+            result <<= 1u;
+            result |= bit;
+        }
+    }
+
+    return result;
+}
